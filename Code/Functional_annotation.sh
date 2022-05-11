89ffd6d9ad9380e56f163b20e0a02ad5ca2755f3
ESiggstedt <siggstedtellen@gmail.com>
2022-05-11 09:46:22 +0200

    Script for doing Functional annotation with eggnogmapper

---
+++ Code/Functional_annotation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2022-2-5
+#SBATCH -M snowy
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 1:00:00
+#SBATCH -J Functional_annotation
+#SBATCH -o Functional_annotation.output
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user ellen.siggstedt.1185@student.uu.se
+
+module load bioinfo-tools
+module load eggNOG-mapper/2.1.5
+
+/sw/bioinfo/eggNOG-mapper/2.1.5/snowy/bin/emapper.py \
+-i /home/ellens/Durian_fruit/Analyses/10_Remove_repeats_DNA/Remove_repeats_DNA/corrected_assembly.fasta.masked \
+-o Functional_annotations \
+--threads 2 \
+--output_dir /home/ellens/Durian_fruit/Analyses/13_Functional_annotation \
+--itype genome
