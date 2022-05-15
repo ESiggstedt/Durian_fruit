fe09571e26ca36605ab103d8e632e7e89b54ec5f
ESiggstedt <siggstedtellen@gmail.com>
2022-05-15 12:01:41 +0200

    modifications

--- Code/Read_counting.sh
+++ Code/Read_counting.sh
@@ -2,8 +2,8 @@
 #SBATCH -A uppmax2022-2-5
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 1:00:00
+#SBATCH -n 8
+#SBATCH -t 6:00:00
 #SBATCH -J Read_counting
 #SBATCH -o Read_counting.output
 #SBATCH --mail-type=ALL
@@ -13,61 +13,61 @@ module load bioinfo-tools
 module load htseq/0.12.4
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6040092_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6040092_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6040093_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6040093_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6040094_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6040094_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6040096_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6040096_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6040097_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6040097_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6156066_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6156066_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6156069_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6156069_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_SRR6156067_scaffold_10_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/SRR6156067_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_trimmed_transcriptomes_scaffold_10_P_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/trimmed_P_counted
 
 /sw/bioinfo/htseq/0.12.4/snowy/bin/htseq-count \
---format=bam --order pos --nprocesses 2 --samout=Read_counting --idattr ID \
+--format=bam --order pos --nprocesses 8 --samout=Read_counting --idattr ID \
 /home/ellens/Durian_fruit/Analyses/11_mapping_DNA_transcriptome/sorted_trimmed_transcriptomes_scaffold_10_U_mappedAligned.out.bam \
 /home/ellens/Durian_fruit/Analyses/12_Structural_annotation/braker/Durio_zibethinus/augustus.hints.gff3 \
 --counts_output /home/ellens/Durian_fruit/Analyses/14_Read_counting/trimmed_U_counted
